747f77b42cc12249cb5ddabc8b49df5ef98cb4d0
ZhenyaMelnichuk <zhenyamlnck212@gmail.com>
2025-04-25 08:17:00 +0300

    add AUTH_TRUST_HOST in env

--- auth.ts
+++ auth.ts
@@ -40,5 +40,6 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
       },
     }),
   ],
-  trustHost: true
+  // trustHost: true
+  // В файл env нужно добавить AUTH_TRUST_HOST=true
 })
